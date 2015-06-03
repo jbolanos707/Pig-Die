2355c0f057334e6f90d055a9db59c669a65439ff
andrew david burt & Ben Cornelis <student@epicodus.com>
2015-06-03 11:20:39 -0700

    allow users to enter names via web interface and display them back.

--- js/scripts.js
+++ js/scripts.js
@@ -22,3 +22,29 @@ function Game(player1, player2) {
 Game.prototype.switchTurn = function() {
   this.currentPlayerIndex = (this.currentPlayerIndex + 1) % 2;
 }
+
+
+$(function() {
+
+  var newGame;
+
+  $("form#new-players").submit(function(event) {
+    event.preventDefault();
+
+    var inputPlayer1 = $("input#player1").val(),
+        inputPlayer2 = $("input#player2").val();
+    newGame = new Game(inputPlayer1, inputPlayer2);
+    $("#pregame").hide();
+    $("#game #player1-info").append(newGame.players[0].name + "<br>" +
+                                    "Score: <span id='0'>0</span>");
+    $("#game #player2-info").append(newGame.players[1].name + "<br>" +
+                                    "Score: <span id='1'>0</span>");
+
+    $("#game").show();
+  });
+
+
+
+
+
+});
